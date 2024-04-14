1837f60de7840cebb64b16c9b6e6dcf6ecab0381
Piotr Zaniewski <piotr@upbound.io>
2024-04-14 14:38:45 +0200

    stylua
    Signed-off-by: Piotr Zaniewski <piotr@upbound.io>

--- plugin/toggler.lua
+++ plugin/toggler.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_user_command('ToggleCommand', function(opts)
-    local args = {}
-    -- Parse options passed as key-value pairs
-    for i = 1, #opts.fargs, 2 do
-        args[opts.fargs[i]] = opts.fargs[i + 1]
-    end
-    require('toggler').toggle_command(args)
+  local args = {}
+  -- Parse options passed as key-value pairs
+  for i = 1, #opts.fargs, 2 do
+    args[opts.fargs[i]] = opts.fargs[i + 1]
+  end
+  require('toggler').toggle_command(args)
 end, { nargs = '*' })
